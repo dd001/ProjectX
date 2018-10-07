27d04e6550d1dcc5c0422280837897b3a9c88dc6
HP\Ciff <androiddeveloper2014!@gmail.com>
2018-10-07 15:54:41 +0530

    base

---
+++ app/src/main/java/com/loststarventures/projectx/projectx/core/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.loststarventures.projectx.projectx.core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.loststarventures.projectx.projectx.R;
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/loststarventures/projectx/projectx/login/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.loststarventures.projectx.projectx.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.loststarventures.projectx.projectx.R;
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
